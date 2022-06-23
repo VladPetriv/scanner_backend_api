f7813d8a671f870151bf325d61fa06819a5e5183
Vlad Petriv <vladpetriv05@gmail.com>
2022-06-23 15:25:59 +0300

    feat: add methods for channel repository

--- internal/store/pg/channel.go
+++ internal/store/pg/channel.go
@@ -1,9 +1,55 @@
 package pg
 
+import (
+	"errors"
+	"fmt"
+
+	"github.com/VladPetriv/scanner_backend_api/internal/model"
+)
+
+var ErrChannelNotFound = errors.New("channel/s not found")
+
 type ChannelRepo struct {
 	db *DB
 }
 
 func NewChannelRepo(db *DB) *ChannelRepo {
 	return &ChannelRepo{db: db}
+}
+
+func (c *ChannelRepo) GetChannelsCount() (int, error) {
+	var count int
+
+	err := c.db.Get(&count, "SELECT COUNT(id) FROM channel;")
+	if err != nil {
+		return count, fmt.Errorf("failed to get count of channel: %w", err)
+	}
+
+	return count, nil
+}
+
+func (c *ChannelRepo) GetChannelsByPage(offset int) ([]model.Channel, error) {
+	channels := make([]model.Channel, 0, 10)
+
+	err := c.db.Select(&channels, "SELECT * FROM channel OFFSET $1 LIMIT 10;", offset)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get channels by page: %w", err)
+	}
+
+	if len(channels) == 0 {
+		return nil, ErrChannelNotFound
+	}
+
+	return channels, nil
+}
+
+func (c *ChannelRepo) GetChannelByName(name string) (*model.Channel, error) {
+	var channel model.Channel
+
+	err := c.db.Get(&channel, "SELECT * FROM channel WHERE name = $1;", name)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get channel by name: %w", err)
+	}
+
+	return &channel, nil
 }
--- internal/store/repository.go
+++ internal/store/repository.go
@@ -1,7 +1,13 @@
 package store
 
+import "github.com/VladPetriv/scanner_backend_api/internal/model"
+
 //go:generate mockery --dir . --name ChannelRepo --output ./mocks
-type ChannelRepo interface{}
+type ChannelRepo interface {
+	GetChannelsCount() (int, error)
+	GetChannelsByPage(offset int) ([]model.Channel, error)
+	GetChannelByName(name string) (*model.Channel, error)
+}
 
 //go:generate mockery --dir . --name UserRepo --output ./mocks
 type UserRepo interface{}
