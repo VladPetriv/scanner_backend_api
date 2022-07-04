86c78d30450b6d143be607759f6c3976bc847070
Vlad Petriv <vladpetriv05@gmail.com>
2022-07-04 10:26:51 +0300

    fix: update message model because have an error while do request to db

--- internal/model/message.go
+++ internal/model/message.go
@@ -1,18 +1,18 @@
 package model
 
 type FullMessage struct {
-	ID              int    `json:"id" db:"messageId"`
-	Title           string `json:"title" db:"messageTitle"`
-	MessageURL      string `json:"messageUrl" db:"messageUrl"`
-	MessageImageURL string `json:"messageImageUrl" db:"messageImageUrl"`
+	ID              int    `json:"id" db:"messageid"`
+	Title           string `json:"title" db:"messagetitle"`
+	MessageURL      string `json:"messageUrl" db:"messageurl"`
+	MessageImageURL string `json:"messageImageUrl" db:"messageimageurl"`
 
-	ChannelName     string `json:"channelName" db:"channelName"`
-	ChannelTitle    string `json:"channelTitle" db:"channelTitle"`
-	ChannelImageURL string `json:"channelImageUrl" db:"channelImageUrl"`
+	ChannelName     string `json:"channelName" db:"channelname"`
+	ChannelTitle    string `json:"channelTitle" db:"channeltitle"`
+	ChannelImageURL string `json:"channelImageUrl" db:"channelimageurl"`
 
-	UserID       int    `json:"userId" db:"userId"`
-	UserFullname string `json:"userFullname" db:"userFullname"`
-	UserImageURL string `json:"userImageUrl" db:"userImageUrl"`
+	UserID       int    `json:"userId" db:"userid"`
+	UserFullname string `json:"userFullname" db:"userfullname"`
+	UserImageURL string `json:"userImageUrl" db:"userimageurl"`
 
 	RepliesCount int          `json:"repliesCount" db:"count"`
 	Replies      []FullReplie `json:"replies"`
